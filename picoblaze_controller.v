955a353ccdb4ca0f91b0aa40105167324e1199ea
Sage Keesler <keeslers@usf.edu>
2022-05-04 13:37:39 -0400

    FSM implemented, needs picoblaze interfacing and audio/memory interfacing

--- picoblaze_controller.v
+++ picoblaze_controller.v
@@ -61,10 +61,7 @@ module picoblaze_controller(
 	input  RS232_Uart_RX,
 	output RS232_Uart_TX 
 );
-
-	wire clk37;
-	wire clk100mhz;
-	wire clk100mhz_2;
+	
 	// REMOVED: data read from/to audio, not switches/LEDs
 	/*
 	input			[7:0]	switches;
@@ -84,8 +81,15 @@ module picoblaze_controller(
 	wire			pb_interrupt;
 	wire			pb_int_ack;
 	
+	// Data wires and registers
+	reg			[25:0] address;
+	reg			[25:0] max_address;
+	wire			[15:0] read_out;
+	wire			[15:0] audio_data;
+	
 	// RAM Interface wires
 	wire			RAM_reset;
+	reg			[25:0] max_RAM_address;
 	
 	// Audio Interface wires
 	wire			aud_reset;
@@ -104,52 +108,6 @@ module picoblaze_controller(
 
 	// REMOVED: LED Driver and control logic (no need for LEDs)
 	
-	// * Picoblaze States
-	reg play, pause, record, delete, delete_all;
-	// * Boolean Flags to hold states.
-	reg is_playing, is_recording, storage_full;
-	// * Menu options to interact with keypad buttons.
-	reg main_menu, play_menu, recording_menu, delete_msg_menu, 
-		del_all_menu, vol_menu;
-	
-	// * Registers used by the FSM.
-	reg play_msg_1, play_msg_2, play_msg_3, play_msg_4, play_msg_5;
-	reg recording_msg_1, recording_msg_2, recording_msg_3, recording_msg_4, recording_msg_5;
-	reg count, del_1, del_2, del_3, del_4, del_5;
-
-	reg [3:0] volume_setting;
-	reg vol_up, vol_down;
-
-	wire [15:0] audio_out;
-
-	myclock wizard
-  	(
-    	.CLK_IN1(clk37), 
-    	.CLK_OUT1(clk100mhz),
-	 	.CLK_OUT2(clk100mhz_2)
-	);
-
-	initial begin
-		play <= 0;
-		pause <= 0;
-		record <= 0;
-		is_playing <= 0;
-		is_recording <= 0;
-		delete <=0;
-		delete_all <= 0;
-		state <= 8'h00;
-		storage_full <= 0;
-		volume_setting <= 1;
-		vol_up <= 0;
-		vol_down <= 0;
-		main_menu <= 1;
-		play_menu <= 0;
-		recording_menu <= 0;
-		delete_msg_menu <= 0;
-		del_all_menu <= 0;
-		vol_menu <= 0;
-	end
-
 	// RAM Interface
 	//
 	// RAM interface uses ACTIVE-HIGH reset
@@ -189,7 +147,7 @@ module picoblaze_controller(
 	assign aud_reset = ~RST;
 	// Audio Interface instantiation
 	sockit_top audio_interface(
-		.OSC_100MHz(clk100mhz_2),	// 100 mhz clock from clock wiz
+		.OSC_100MHz(/*<FILL_IN>*/),	// 100 mhz clock from clock wiz
 		.AUD_ADCLRCK(AUD_ADCLRCK),
 		.AUD_ADCDAT(AUD_ADCDAT),
 		.AUD_DACLRCK(AUD_DACLRCK),
@@ -243,7 +201,7 @@ module picoblaze_controller(
 		.interrupt(pb_interrupt),
 		.interrupt_ack(pb_int_ack),
 		.reset(pb_reset),
-		.clk(clk100mhz)	// Clock signal generated from RAM interface
+		.clk(/*<FILL_IN>*/)	// Clock signal generated from RAM interface
 	);	
 	// PB I/O selection/routing
 	//
@@ -255,12 +213,7 @@ module picoblaze_controller(
 	// These signals are effectively "write enable" lines for the UART and LED
 	// Driver modules. They must be asserted when PB is outputting to the
 	// corresponding port
-	assign play_menu = kypd_row0 & kypd_col0;
-	assign recording_menu = kypd_row0 & kypd_col1;
-	assign delete_msg_menu = kypd_row0 & kypd_col2;
-	assign del_all_menu = kypd_row1 & kypd_col0;
-	assign vol_menu = kypd_row1 & kypd_col1;
-	assign write_to_leds = pb_write_strobe & (pb_port_id == 8'h06);
+	assign write_to_leds = pb_write_strobe & (pb_port_id == 8'h01);
 	assign write_to_uart = pb_write_strobe & (pb_port_id == 8'h03);
 	//
 	// Handle PicoBlaze Input Port Logic
@@ -282,17 +235,6 @@ module picoblaze_controller(
 			// Set pb input port to appropriate value
 			case(pb_port_id)
 				// TODO: INTERFACE PICOBLAZE PORTS
-				8'h00: pb_in_port <= main_menu;
-				8'h01: pb_in_port <= play_menu;
-				8'h02: pb_in_port <= uart_data_rx;
-				8'h03: pb_in_port <= recording_menu;
-				8'h04: pb_in_port <= {7'b0000000,uart_data_present};
-				8'h05: pb_in_port <= {7'b0000000,uart_buffer_full};
-				8'h06: pb_in_port <= delete_msg_menu;
-				8'h07: pb_in_port <= del_all_menu;
-				8'h08: pb_in_port <= vol_menu;
-				8'h09: pb_in_port <= is_playing;
-				8'h0A: pb_in_port <= is_recording;
 				default: pb_in_port <= 8'h00;
 			endcase
 			// Set up acknowledge/enable signals.
@@ -303,78 +245,6 @@ module picoblaze_controller(
 			// signal high for corresponding ports, as needed. Most input
 			// ports will not need this.
 			read_from_uart <= pb_read_strobe & (pb_port_id == 8'h04);
-			// Initialize registers
-			if (write_to_leds) begin
-				main_menu <= (pb_out_port == 8'h00);
-				play_menu <= (pb_out_port == 8'h01);
-				recording_menu <= (pb_out_port == 8'h03);
-				delete_msg_menu <= (pb_out_port == 8'h06);
-				del_all_menu <= (pb_out_port == 8'h07);
-				vol_menu <= (pb_out_port == 8'h08);
-				is_playing <= (pb_out_port == 8'h09);
-				is_recording <= (pb_out_port == 8'h0A);
-			end
-			// Main menu
-			if (main_menu) begin
-				if (play_menu) begin
-					play_msg_1 <= (pb_out_port == 8'h01);
-					play_msg_2 <= (pb_out_port == 8'h03);
-					play_msg_3 <= (pb_out_port == 8'h06);
-					play_msg_4 <= (pb_out_port == 8'h07);
-					play_msg_5 <= (pb_out_port == 8'h08);
-				end
-				else if (recording_menu) begin
-					recording_msg_1 <= (pb_out_port == 8'h01);
-					recording_msg_2 <= (pb_out_port == 8'h03);
-					recording_msg_3 <= (pb_out_port == 8'h06);
-					recording_msg_4 <= (pb_out_port == 8'h07);
-					recording_msg_5 <= (pb_out_port == 8'h08);
-				end
-				else if (delete_msg_menu) begin
-					del_1 <= (pb_out_port == 8'h01);
-					del_2 <= (pb_out_port == 8'h03);
-					del_3 <= (pb_out_port == 8'h06);
-					del_4 <= (pb_out_port == 8'h07);
-					del_5 <= (pb_out_port == 8'h08);
-				end
-				else if (del_all_menu) begin
-					count = 1;
-					while (count < 6) begin
-						case (count)
-							1: pb_in_port <= del_1;
-							2: pb_in_port <= del_2;
-							3: pb_in_port <= del_3;
-							4: pb_in_port <= del_4;
-							5: pb_in_port <= del_5;
-						endcase
-						count = count + 1;
-					end
-				end
-				else if (vol_menu) begin
-					vol_up <= (pb_out_port == 8'h01);
-					vol_down <= (pb_out_port == 8'h03);
-				end
-			end
-			else if (play_menu) begin
-				case (play_menu)
-					play_msg_1: 
-					play_msg_2: 
-					play_msg_3: 
-					play_msg_4: 
-					play_msg_5: pb_in_port <= is_playing;
-					default: pb_in_port <= play_menu;
-				endcase
-			end
-			else if (recording_menu) begin
-				case (recording_menu)
-					recording_msg_1: 
-					recording_msg_2: 
-					recording_msg_3: 
-					recording_msg_4: 
-					recording_msg_5: pb_in_port <= is_recording;
-					default: pb_in_port <= recording_menu;
-				endcase
-			end
 		end
 	end
 
@@ -389,19 +259,234 @@ module picoblaze_controller(
 	clock_divider divider (.clk(OSC_100MHz), .rst(pb_reset), .clk_div(clk_div));
 	//
 	// Debouncer instantiations for key pads
-	debouncer row0 (.clk(OSC_100MHz), .clk_div(clk_div), .in(KYPD_ROW[0]), .out(kypd_row0));
-	debouncer row1 (.clk(OSC_100MHz), .clk_div(clk_div), .in(KYPD_ROW[1]), .out(kypd_row1));
-	debouncer row2 (.clk(OSC_100MHz), .clk_div(clk_div), .in(KYPD_ROW[2]), .out(kypd_row2));
-	debouncer row3 (.clk(OSC_100MHz), .clk_div(clk_div), .in(KYPD_ROW[3]), .out(kypd_row3));
-	debouncer col0 (.clk(OSC_100MHz), .clk_div(clk_div), .in(KYPD_COL[0]), .out(kypd_col0));
-	debouncer col1 (.clk(OSC_100MHz), .clk_div(clk_div), .in(KYPD_COL[1]), .out(kypd_col1));
-	debouncer col2 (.clk(OSC_100MHz), .clk_div(clk_div), .in(KYPD_COL[2]), .out(kypd_col2));
-	debouncer col3 (.clk(OSC_100MHz), .clk_div(clk_div), .in(KYPD_COL[3]), .out(kypd_col3));
+	debouncer row0 (.clk(OSC_100MHz), .clock_div(clk_div), .in(KYPD_ROW[0]), .out(kypd_row0));
+	debouncer row1 (.clk(OSC_100MHz), .clock_div(clk_div), .in(KYPD_ROW[1]), .out(kypd_row1));
+	debouncer row2 (.clk(OSC_100MHz), .clock_div(clk_div), .in(KYPD_ROW[2]), .out(kypd_row2));
+	debouncer row3 (.clk(OSC_100MHz), .clock_div(clk_div), .in(KYPD_ROW[3]), .out(kypd_row3));
+	debouncer col0 (.clk(OSC_100MHz), .clock_div(clk_div), .in(KYPD_COL[0]), .out(kypd_col0));
+	debouncer col1 (.clk(OSC_100MHz), .clock_div(clk_div), .in(KYPD_COL[1]), .out(kypd_col1));
+	debouncer col2 (.clk(OSC_100MHz), .clock_div(clk_div), .in(KYPD_COL[2]), .out(kypd_col2));
+	debouncer col3 (.clk(OSC_100MHz), .clock_div(clk_div), .in(KYPD_COL[3]), .out(kypd_col3));
 	
 	// FSM code block for menu controls and module interfacing
 	//
 	// Wires and Registers for state functions
 	wire	RAM_status;		// 1 if RAM is ready for R/W
-	
+	wire  audio_ready;
+	wire  data_ready;
+	reg   write_en;
+	reg   write_req;
+	wire  write_ack;
+	reg   read_req;
+	reg   read_ack;
+	reg   [2:0] volume;
+	reg   mem_full;
+	//
+	// Registers for tracks (track[26] is valid bit)
+	reg  [26:0] track1;
+	reg  [26:0] track2;
+	reg  [26:0] track3;
+	reg  [26:0] track4;
+	reg  [26:0] track5;
+	reg  [2:0] track_sel;
+	//
+	// Keypad controls
+	wire kypd1, kypd2, kypd3, kypd4, kypd5, kypd6, kypd0, kypdA, kypdB;
+	assign kypd1 = (row0 & col0);
+	assign kypd2 = (row0 & col1);
+	assign kypd3 = (row0 & col2);
+	assign kypdA = (row0 & col3);
+	assign kypd4 = (row1 & col0);
+	assign kypd5 = (row1 & col1);
+	assign kypd6 = (row1 & col2);
+	assign kypdB = (row1 & col3);
+	assign kypd0 = (row3 & col0);
+	//
+	// State encoding
+	parameter init_state			=	4'b0000;
+	parameter menu					=	4'b0001;
+	parameter play_menu			=	4'b0010;
+	parameter play_state			=	4'b0011;
+	parameter record_state		=	4'b0100;
+	parameter delete_menu		=	4'b0101;
+	parameter delete_state		=	4'b0110;
+	parameter delete_all			=	4'b0111;
+	parameter mem_full_state	=	4'b1000;
+	parameter change_volume		=	4'b1001;
+	parameter volume_up			=	4'b1010;
+	parameter volume_down		=	4'b1100;
+	//
+	// State register
+	reg [3:0] state = init_state;
+	//
+	// FSM logic
+	always@ (posedge OSC_100MHz) begin
+		// Reset case
+		if (pb_reset) begin
+			write_en 	<= 0;
+			write_req 	<= 0;
+			read_req 	<= 0;
+			read_ack 	<= 0;
+			volume 		<= 0;
+			address 		<= 0;
+			state 		<= init_state;
+		end
+		// State logic
+		// TODO: INSERT PICOBLAZE PORT COMMANDS
+		else begin
+			case (state)
+				// Initialization state
+				init_state: begin
+					write_en 	<= 0;
+					write_req 	<= 0;
+					read_req 	<= 0;
+					read_ack 	<= 0;
+					volume 		<= 0;
+					address 		<= 0;
+					mem_full 	<= 0;
+					track1 		<= 0;
+					track2 		<= 0;
+					track3 		<= 0;
+					track4 		<= 0;
+					track5		<= 0;
+					state			<= menu;
+				end
+				// Main menu
+				menu: begin
+					if (kypd1)					// Play message
+						state <= play_menu;
+					else if (kypd2)			// Record message
+						state <= record_state;
+					else if (kypd3)			// Delete message
+						state <= delete_menu;
+					else if (kypd4)			// Delete all messages
+						state <= delete_all;
+					else if (kypd5)			// Volume control
+						state <= change_volume;
+					else							// Loop on menu until selection is made
+						state <= menu;
+				end
+				// Play menu - select a track to play
+				play_menu: begin
+					if (kypd1) begin
+						track_sel 	<= 3'b001;
+						state 		<= play_state;
+					end
+					else if (kypd2) begin
+						track_sel	<= 3'b010;
+						state			<= play_state;
+					end
+					else if (kypd3) begin
+						track_sel	<= 3'b011;
+						state			<= play_state;
+					end
+					else if (kypd4) begin
+						track_sel	<= 3'b100;
+						state			<= play_state;
+					end
+					else if (kypd5) begin
+						track_sel	<= 3'b101;
+						state			<= play_state;
+					end
+					else if (kypd0)			// Exit back to the main menu
+						state 		<= menu;
+					else							// Loop on menu until selection is made
+						state			<= play_menu;
+				end
+				// Play state - begin playing audio from selected track
+				play_state: begin
+					// TODO: AUDIO PLAYBACK LOGIC
+				end
+				// Record state
+				record_state: begin
+					// TODO: RECORD AUDIO LOGIC
+					if (mem_full)
+						state <= mem_full_state;
+					else
+						state <= menu;
+				end
+				// Delete menu - select a track to delete
+				delete_menu: begin
+					if (kypd1) begin
+						track_sel	<= 3'b001;
+						state			<= delete_state;
+					end
+					else if (kypd2) begin
+						track_sel	<= 3'b010;
+						state			<= delete_state;
+					end
+					else if (kypd3) begin
+						track_sel	<= 3'b011;
+						state			<= delete_state;		
+					end
+					else if (kypd4) begin
+						track_sel	<= 3'b100;
+						state			<= delete_state;
+					end
+					else if (kypd5) begin
+						track_sel	<= 3'b101;
+						state			<= delete_state;
+					end
+					else if (kypd0)			// Exit back to the main menu
+						state			<= menu;
+					else							// Loop on menu until selection is made
+						state			<= delete_menu;
+				end
+				// Delete state - sets selected track's valid bit to 0
+				delete_state: begin
+					if (track_sel == 3'b001)
+						track1[26] <= 1'b0;
+					else if (track_sel == 3'b010)
+						track2[26] <= 1'b0;
+					else if (track_sel == 3'b011)
+						track3[26] <= 1'b0;
+					else if (track_sel == 3'b100)
+						track4[26] <= 1'b0;
+					else if (track_sel == 3'b101)
+						track5[26] <= 1'b0;
+					state <= menu;				// Return to main menu after delete is performed
+				end
+				// Delete all state - sets every track's valid bit to 0
+				delete_all: begin
+					track1[26] <= 1'b0;
+					track2[26] <= 1'b0;
+					track3[26] <= 1'b0;
+					track4[26] <= 1'b0;
+					track5[26] <= 1'b0;
+					state 	  <= menu;
+				end
+				// Memory full state - displays picoblaze message that memory is full and returns to menu
+				mem_full_state: begin
+					// TODO: INSERT PICOBLAZE PORT
+					state <= menu;
+				end
+				// Change volume state - select 'up' or 'down' to change volume
+				change_volume: begin
+					if (kypdA)					// Keypad A used as 'volume up' button
+						state <= volume_up;
+					else if (kypdB)			// Keypad B used as 'volume down' button
+						state <= volume_down;
+					else if (kypd0)			// Exit back to main menu
+						state <= menu;
+					else							// Loop on menu until selection is made
+						state <= change_volume;
+				end
+				// Volume up state - increases volume by 1 if it is not already at max
+				volume_up: begin
+					if (volume < 3'b111)
+						volume <= volume + 1;
+					// TODO: PICOBLAZE PORT FOR VOLUME INDICATOR
+					state <= change_volume;
+				end
+				// Volume down state - decrease volume by 1 if it is not already at min
+				volume_down: begin
+					if (volume > 3'b000)
+						volume <= volume + 1;
+					// TODO: PICOBLAZE PORT FOR VOLUME INDICATOR
+					state <= change_volume;
+				end
+			endcase
+		end
+	end
 	
 endmodule
